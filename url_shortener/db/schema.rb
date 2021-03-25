b1823efe300acd669ea8946823e1fb41dd516ba2
fdiengott <freddiengott@gmail.com>
2021-03-25 17:15:19 -0400

    Add migrations

--- url_shortener/db/schema.rb
+++ url_shortener/db/schema.rb
@@ -10,16 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_25_203412) do
+ActiveRecord::Schema.define(version: 2021_03_25_210145) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.text "username", null: false
     t.text "email", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
