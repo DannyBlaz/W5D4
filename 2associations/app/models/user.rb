574b081540d58cc81744b28b0859b824f0f80731
fdiengott <freddiengott@gmail.com>
2021-03-25 15:08:20 -0400

    Add User#enrollments and #enrolled_courses, and tested associations

--- 2associations/app/models/user.rb
+++ 2associations/app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+
+  #enrollments
+  has_many :enrollments,
+    primary_key: :id,
+    foreign_key: :course_id,
+    class_name: :Enrollment
+  
+  #enrolled_courses
+  has_many :enrolled_courses,
+    through: :enrollments,
+    source: :course
+  
 end
