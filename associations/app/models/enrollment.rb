fef61f515cfdbf8300b14c5274b835a8b79165d4
fdiengott <freddiengott@gmail.com>
2021-03-25 14:47:41 -0400

    delete bugged associations folder tree

--- associations/app/models/enrollment.rb
+++
@@ -1,9 +0,0 @@
-class Enrollment < ApplicationRecord
-
-    #associaions for user and course
-    belongs_to :course
-        primary_key: :id
-        foreign_key: :course_id
-        class_name: :Course
-
-end
